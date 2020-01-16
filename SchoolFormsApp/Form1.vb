0709edc890c1621bb2af041404fc49a0bc53ee77
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2020-01-16 12:26:58 -0600

    Added Labels and Button to form.

--- SchoolFormsApp/Form1.vb
+++ SchoolFormsApp/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles TextName.Click
 
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles TxtAddress.Click
+
+    End Sub
 End Class
